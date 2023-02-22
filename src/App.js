0876692a771a344c32b3d484aa99242a54c4a7d1
hardikd-zignuts <hardikd@zignuts.com>
2023-02-22 14:05:09 +0530

    Link Route Path with Navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import About from './components/About.jsx'
 import Home from './components/Home.jsx'
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
